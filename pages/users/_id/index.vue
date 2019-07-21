237439580d8b71ea25121024bbcf3285c9a2db90
AdinoWayne <gnurt10@gmail.com>
2019-07-21 19:57:04 +0700

    init vue lession 1

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>This is user's {{$route.params.id}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    validate(context) {
+        return /^\d+$/.test(context.params.id)
+    }
+}
+</script>
